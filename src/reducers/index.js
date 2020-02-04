990381a451b721d148d3b1f5e1345b498332fc98
dinoxas <dhassiotis07@gmail.com>
2020-02-04 19:32:51 +0000

    added and implemented react-redux-loading bar

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import authedUser from "../reducers/authedUser";
 import users from "../reducers/users";
 import polls from "../reducers/polls";
 import { combineReducers } from "redux";
+import { loadingBarReducer } from "react-redux-loading";
 
 export default combineReducers({
   authedUser,
   users,
-  polls
+  polls,
+  loadingBar: loadingBarReducer
 });
