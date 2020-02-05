ebeb07aa75777ca791da7a51941206e973f96b82
dinoxas <dhassiotis07@gmail.com>
2020-02-05 12:19:25 +0000

    created leaderboard component and ui

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { handleInitialData } from "../actions/shared";
 import { connect } from "react-redux";
 import Dashboard from "./Dashboard";
+import Leaderboard from "./Leaderboard";
 import LoadingBar from "react-redux-loading";
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
     return (
       <div>
         <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
+        {this.props.loading === true ? null : <Leaderboard />}
       </div>
     );
   }
