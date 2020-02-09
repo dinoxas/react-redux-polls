221e2dbc2089b709d8e715bbc4299852daceeec2
dinoxas <dhassiotis07@gmail.com>
2020-02-09 18:55:14 +0000

    created answer action creator and added answer state to store

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,6 @@
 import { RECEIVE_USERS } from "../actions/users";
 import { ADD_POLL } from "../actions/polls";
+import { ADD_ANSWER } from "../actions/answers";
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -21,6 +22,19 @@ export default function users(state = {}, action) {
         }
       };
     }
+
+    case ADD_ANSWER: {
+      const user = state[action.authedUser];
+
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...user,
+          answers: user.answers.concat([action.id])
+        }
+      };
+    }
+
     default:
       return state;
   }
